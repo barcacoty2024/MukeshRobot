dbc50d904453ca4373e738f005064e35cc538079
barcacoty2024 <158706333+barcacoty2024@users.noreply.github.com>
2024-05-09 03:11:09 +0700

    __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -49,7 +49,7 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "worldwide_friend_zone")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "musik_supportdan")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -61,13 +61,13 @@ if ENV:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
     try:
-        BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
+        BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "5779185981").split())
     except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "2145093972").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "5779185981").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
@@ -82,7 +82,7 @@ if ENV:
         raise Exception("Your tiger users list does not contain valid integers.")
 
     try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
+        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "5779185981").split())
     except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
