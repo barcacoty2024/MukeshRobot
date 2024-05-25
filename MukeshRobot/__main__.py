6a1d0ce9d49419c79cf98dc09057ac82312adfcd
barcacoty2024 <158706333+barcacoty2024@users.noreply.github.com>
2024-05-26 06:01:14 +0700

    __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -81,8 +81,8 @@ PM_START_TEXT = """
 ʙᴏᴛ ᴜɴᴛᴜᴋ ᴍᴇɴɢᴇʟᴏʟᴀ ᴅᴀɴ ᴍᴇᴍᴜᴛᴀʀ ᴍᴜꜱɪᴋ
 ᴅɪɢʀᴜᴘ ᴀɴᴅᴀ ᴅᴇɴɢᴀɴ ʙᴇʀʙᴀɢᴀɪ ꜰɪᴛᴜʀ.!*
 ─────────────────
-   *➻ ᴘᴇɴɢɢᴜɴᴀ ʙᴏᴛ »* {}
-   *➻ ᴛᴇʟᴀʜ ᴅɪ ɢᴜɴᴀᴋᴀɴ ᴅɪ »* {} ɢʀᴏᴜᴘs
+   *➻ ᴜꜱᴇʀꜱ »* {}
+   *➻ ᴄʜᴀᴛꜱ »* {} 
 ─────────────────
 ᴅᴇᴠ: @mhmdwldnnnn
 """
@@ -1063,12 +1063,12 @@ def main():
         try:
             dispatcher.bot.send_photo(
                 f"@musik_supportdan",
-                photo=f"https://telegra.ph//file/7427929e1e589ed76e183.jpg",
+                photo=f"https://telegra.ph//file/07b821fb75b8ebd4a3e58.jpg",
                 caption=f"""
 ✨{BOT_NAME} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ.
 ━━━━━━━━━━━━━
-๏ **ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ʙʏ :** [ᴡɪʟᴅᴀɴ](tg://user?id=5779185981)
-๏ ** ᴅᴇᴠᴇʟᴏᴘᴇʀ :** [ᴡɪʟᴅᴀɴ](https://t.me/mhmdwldnnnn)
+๏ **ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ʙʏ :** [ᴅᴀɴ](tg://user?id=5779185981)
+๏ ** ᴅᴇᴠᴇʟᴏᴘᴇʀ :** [ᴅᴀɴ](https://t.me/mhmdwldnnnn)
 ๏ ** ꜱᴜᴘᴘᴏʀᴛ :** [ꜱᴛᴏʀᴇ](https://t.me/Disney_storeDan)
 ━━━━━━━━━━━━━
 """,reply_markup=x,
